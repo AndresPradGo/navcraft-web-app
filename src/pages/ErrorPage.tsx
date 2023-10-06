568b1778ddaf8e919925e06e5eb630f7f1af8f6e
Andres Pradilla <ae.pradillagomez@gmail.com>
2023-10-06 14:11:05 -0700

    Add error handling

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,79 @@
+import { useRouteError, isRouteErrorResponse } from "react-router-dom";
+import { TbHome } from "react-icons/tb";
+
+import { styled } from "styled-components";
+
+import Button from "../components/common/button";
+
+const HtmlContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+`;
+
+const HtmlTextContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const HtmlTitle = styled.h1`
+  color: var(--color-white);
+  font-size: 24px;
+  display: inline-block;
+  border-right: 1px solid var(--color-white);
+  padding-right: 23px;
+  margin: 0 20px 0 0;
+`;
+
+const HtmlText = styled.p`
+  display: inline-block;
+  font-size: 16px;
+  color: var(--color-white);
+  text-align: center;
+`;
+
+const HomeIcon = styled(TbHome)`
+  font-size: 20px;
+`;
+
+const ErrorPage = () => {
+  const error = useRouteError();
+  const errorCode = isRouteErrorResponse(error) ? "400" : "500";
+  const errorMessage = isRouteErrorResponse(error)
+    ? "Page not found!"
+    : "An unexpected error occurred! We're looking into it.";
+
+  return (
+    <HtmlContainer>
+      <HtmlTextContainer>
+        <HtmlTitle>{errorCode}</HtmlTitle>
+        <HtmlText>{errorMessage}</HtmlText>
+      </HtmlTextContainer>
+      <Button
+        href="/flights"
+        color="var(--color-primary-dark)"
+        hoverColor="var(--color-primary-dark)"
+        backgroundColor="var(--color-contrast)"
+        backgroundHoverColor="var(--color-contrast-hover)"
+        shadow={true}
+        spaceChildren="space-evenly"
+        width={140}
+      >
+        HOME PAGE
+        <HomeIcon />
+      </Button>
+    </HtmlContainer>
+  );
+};
+
+export default ErrorPage;
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -5,11 +5,13 @@ import Flights from "../pages/Flights";
 import Layout from "../pages/layout";
 import Users from "../pages/Users";
 import Waypoints from "../pages/Waypoints";
+import ErrorPage from "../pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <Navigate to="/flights" /> },
       { path: "flights", element: <Flights /> },
