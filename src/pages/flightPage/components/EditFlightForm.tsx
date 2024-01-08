8d22c3ea6baa8b180fa4ddf70a1f87a861224fe9
Andres Pradilla <ae.pradillagomez@gmail.com>
2024-01-08 15:10:46 -0800

    Adjust edit-flight-form validation squema

--- src/pages/flightPage/components/EditFlightForm.tsx
+++ src/pages/flightPage/components/EditFlightForm.tsx
@@ -268,10 +268,12 @@ export const schema = z.object({
     .min(0, { message: "Must be greater than zero" }),
   briefing_radius_nm: z
     .number({ invalid_type_error: "Enter a number" })
-    .min(0, { message: "Must be a positive number" }),
+    .min(5, { message: "Must be greater than 5" })
+    .max(50, { message: "Must be less than 50" }),
   diversion_radius_nm: z
     .number({ invalid_type_error: "Enter a number" })
-    .min(0, { message: "Must be a positive number" }),
+    .min(5, { message: "Must be greater than 5" })
+    .max(100, { message: "Must be less than 100" }),
 });
 
 export type EditFlightData = z.infer<typeof schema>;
