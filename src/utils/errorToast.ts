4b4c2947265bb7535b344979c15bc31d802417b9
Andres Pradilla <ae.pradillagomez@gmail.com>
2024-01-15 16:23:42 -0800

    Update error messages

--- src/utils/errorToast.ts
+++ src/utils/errorToast.ts
@@ -15,6 +15,17 @@ const errorToast = (error: APIClientError) => {
                 progress: undefined,
                 theme: "dark",
             });
+        } else if (typeof error.response.data === "string") {
+            toast.error(error.response.data, {
+                position: "top-center",
+                autoClose: 10000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+            });
         } else {
             toast.error("There's some invalid data in your request, please make sure all data is consistent with the expected data-format.", 
             {
