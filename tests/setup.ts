c2363458cf897e61d89edb7a66bc01de5ea8877f
Andres Pradilla <ae.pradillagomez@gmail.com>
2024-03-29 14:17:25 -0700

    Fix config files with lint

--- tests/setup.ts
+++ tests/setup.ts
@@ -4,6 +4,7 @@ Object.defineProperty(window, 'matchMedia', {
   writable: true,
   value: vi.fn().mockImplementation((query) => ({
     matches: false,
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     media: query,
     onchange: null,
     addListener: vi.fn(), // deprecated
